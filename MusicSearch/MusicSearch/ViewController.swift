bb10f34793b078f7a542247f37de03da2d0e4522
Jhwan2 <Jhwan2@github.com>
2022-11-08 21:54:26 +0900

    musicsearch1

---
+++ MusicSearch/MusicSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicSearch
+//
+//  Created by 이주환 on 2022/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
